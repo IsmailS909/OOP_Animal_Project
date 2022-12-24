16b517facf8f5340f86be3ffa9cb0e1bff696ae6
Coding Rooms <git@codingrooms.io>
2022-12-24 22:23:04 +0000

    testing, this should save to repository

---
+++ my_OOP_Code/Eightmoves.java
@@ -0,0 +1,12 @@
+package my_OOP_Code;
+
+public class Eightmoves{
+public int _var;
+    public Eightmoves(int var){
+        _var = var;
+}
+public void test(){
+    System.out.println(_var + " this works yay");
+
+}
+}
--- my_OOP_Code/Main.java
+++ my_OOP_Code/Main.java
@@ -7,6 +7,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
 
+        Eightmoves first = new Eightmoves(100);
+        first.test();
     }
 
 }
